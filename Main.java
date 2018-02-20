f874ab50f57611baca44c3fb51ad157b8c0e0d9d
Angel <angel.carrillo1@upr.edu>
2018-02-20 11:11:10 -0400

    Added Area calculation and half area and fixing mistakes

---
+++ Main.java
@@ -0,0 +1,14 @@
+class Main{
+	public static void  main(String[] args){
+		System.out.println("Hello world");
+	}
+	
+	private void Area(double r){
+		double result = 3.14 * r * r;
+	}
+
+	private void halfArea(double r){
+		double result =(3.14 * r * r) / 2;
+	}
+}
+
